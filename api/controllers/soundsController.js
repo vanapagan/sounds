2e0d51a149bbe95efb048c5d3bde84bb623258dd
Kristo Palo <kristopalo@hotmail.com>
2017-05-11 20:36:17 +0300

    front-end file upload implemented

--- api/controllers/soundsController.js
+++ api/controllers/soundsController.js
@@ -24,24 +24,6 @@ exports.add_new_sound = function (req, res) {
       console.log('big error');
       res.send(err);
 
-    // successful
-    var part = req.files.filefield;
-
-    var writeStream = gfs.createWriteStream({
-      filename: part.name,
-      mode: 'w',
-      content_type: part.mimetype
-    });
-
-    writeStream.on('close', function () {
-      return res.status(200).send({
-        message: 'Success'
-      });
-    });
-
-    writeStream.write(part.data);
-    writeStream.end();
-
     res.json(sound);
   });
 };
--- public/app.js
+++ public/app.js
@@ -1,6 +1,60 @@
 var app = angular.module('app', ['ngMaterial']);
 
-app.controller('mainController', function ($scope, $http) {
+app.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+
+            element.bind('change', function () {
+                scope.$apply(function () {
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
+
+app.service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function (name, file, uploadUrl) {
+        var fd = new FormData();
+        fd.append('name', file);
+        fd.append('sound', 'name' + '.wav');
+        fd.append('file', file);
+        $http.post(uploadUrl, fd, {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        }).then(function successCallback(response) {
+            console.log('SUCCESS');
+        }, function errorCallback(response) {
+            console.log('ERROR');
+        });
+    }
+}]);
+
+app.controller('mainController', function ($scope, $http, fileUpload) {
+
+    $scope.uploadFile = function () {
+        var name = $scope.name;
+        var file = $scope.myFile;
+        console.log('file is ');
+        console.dir(file);
+        var uploadUrl = "/sounds";
+        fileUpload.uploadFileToUrl(name, file, uploadUrl);
+    };
+
+    $scope.addFile = function () {
+        uploadFile($scope.files,
+            function (msg) // success
+            {
+                console.log('uploaded');
+            },
+            function (msg) // error
+            {
+                console.log('error');
+            });
+    }
 
     $scope.sounds = [];
 
@@ -44,4 +98,5 @@ app.controller('mainController', function ($scope, $http) {
         });
     };
 
-}); 
+});
+
